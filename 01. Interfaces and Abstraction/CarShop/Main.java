97168a024a63a5f215f4181fadfbbf674c6c82c4
MBenina <magdalina.benina@gmail.com>
2017-05-17 07:38:36 +0300

    Update Main.java

--- 01. Interfaces and Abstraction/CarShop/Main.java
+++ 01. Interfaces and Abstraction/CarShop/Main.java
@@ -2,7 +2,7 @@ package ex2.CarShop;
 
 public class Main {
 
-	public static void main(String[] args) {
+	/*public static void main(String[] args) {
 	    Car seat = new Seat("Leon", "gray", 110, "Spain");
 
 	    System.out.println(String.format(
@@ -10,8 +10,26 @@ public class Main {
 	            seat.getModel(),
 	            seat.getColor(),
 	            seat.getHorsePower()));
-	    System.out.println(seat.toString());
+	    System.out.println(seat.toString()); 
+	} */
+	
+	public static void main(String[] args) {
+	    Sellable seat = new Seat("Leon", "Gray", 110, "Spain", 11111.1);
+	    Rentable audi = new Audi("Audi", "Gray", 110, "Spain", 3, 99.9);
+
+	    printCarInfo(seat);
+	    printCarInfo(audi);
 	}
+
+	private static void printCarInfo(Car car) {
+	    System.out.println(String.format(
+	            "%s is %s color and have %s horse power",
+	            car.getModel(),
+	            car.getColor(),
+	            car.getHorsePower()));
+	    System.out.println(car.toString());
+	}
+
 
 
 }
