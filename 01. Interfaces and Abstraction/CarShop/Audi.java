4a97927e7331a06727aad03d948b4d98fed9bfdf
MBenina <magdalina.benina@gmail.com>
2017-05-17 07:37:43 +0300

    Update Audi.java

--- 01. Interfaces and Abstraction/CarShop/Audi.java
+++ 01. Interfaces and Abstraction/CarShop/Audi.java
@@ -5,38 +5,79 @@ public class Audi implements Rentable
 	private String  model;
 	private String color;
 	private int horsePower;
-	private Integer minRentDays;
+	private String countryProduced;
+	private int minRentDays;
 	private Double pricePerDay;   
 
+	public Audi(String  model, String color, int horsePower, String countryProduced, int minRentDays, Double pricePerDay) 
+	{
+		this.setModel(model);
+		this.setColor(color);
+		this.setHorsePower(horsePower);
+		this.setCountryProduced(countryProduced);
+		this.setMinRentDays(minRentDays); 
+		this.setPricePerDay(pricePerDay);
+	}
 	@Override
 	public String getModel() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return this.model;
 	}
 
 	@Override
 	public String getColor() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return this.color;
 	}
 
 	@Override
 	public int getHorsePower() {
-		// TODO Auto-generated method stub
-		return 0;
+		
+		return this.horsePower;
 	}
 
 	@Override
 	public Integer getMinRentDay() {
-		// TODO Auto-generated method stub
-		return null;
+		
+		return this.minRentDays;
 	}
 
 	@Override
-	public Double pricePerDay() {
-		// TODO Auto-generated method stub
-		return null;
+	public Double getpricePerDay() {
+		
+		return this.pricePerDay;
+	}
+	
+	private void setModel(String model) {
+		this.model = model;
+	}
+	
+	private void setColor(String color) {
+		this.color = color;
 	}
 
+	private void setHorsePower(int horsePower) {
+		this.horsePower = horsePower;
+	}
 	
+	 private void setCountryProduced(String countryProduced) 
+	 {
+		 if(countryProduced.length() <= 2)
+		 {
+			 throw new IllegalArgumentException("The name of the country is too short!");
+		 }
+			 
+		this.countryProduced = countryProduced;
+	 }
+	
+	private void setMinRentDays(int minRentDays) {
+		this.minRentDays = minRentDays;
+	}
+	
+	private void setPricePerDay(Double pricePerDay) {
+		this.pricePerDay = pricePerDay;
+	}
+	
+	
+
 }
